beac4113716d2c800f5f181eab7d34c94e150251
4daysonend <mylespetillo@gmail.com>
2024-08-19 10:14:40 -0400

    first commit

--- weather.py
+++ weather.py
@@ -5,6 +5,7 @@ import os
 
 load_dotenv()
 
+
 def get_current_weather(city="New York"):
 
     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
@@ -13,14 +14,18 @@ def get_current_weather(city="New York"):
 
     return weather_data
 
+
 if __name__ == "__main__":
     print('\n*** Get Current Weather Conditions ***\n')
 
     city = input("\nPlease enter a city name: ")
 
+    # check for empty strings or string with only spaces
+
+    if not bool(city.strip()):
+        city = "New York"
+
     weather_data = get_current_weather(city)
 
     print("\n")
     pprint(weather_data)
-
-
